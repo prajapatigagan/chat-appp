ec022516cfbf1e93f30df3dd9a638789a4a9ea07
prajapatigagan <gaganprajapati121@gmail.com>
2026-07-04 23:40:46 +0530

    updates

--- apps/api/src/main/java/com/gagan/chat/controller/ChatController.java
+++ apps/api/src/main/java/com/gagan/chat/controller/ChatController.java
@@ -18,7 +18,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:3000")
+// @CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://chat-appp-web-five.vercel.app"})
 public class ChatController {
 
     private final RoomRepository roomRepository;
